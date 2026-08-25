1450fe5e6cedca22132bd9c919547ef75323f16d
harshvardhansutar44 <harshvardhansutar5050@gmail.com>
2026-08-25 17:36:25 +0530

    Update terraform.sh

--- scripts/terraform.sh
+++ scripts/terraform.sh
@@ -5,17 +5,19 @@
 sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
 
 # Add HashiCorp GPG key
+sudo mkdir -p -m 755 /etc/apt/keyrings
+
 wget -O- https://apt.releases.hashicorp.com/gpg | \
 gpg --dearmor | \
-sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+sudo tee /etc/apt/keyrings/hashicorp-archive-keyring.gpg > /dev/null
 
 # Verify the key fingerprint
 gpg --no-default-keyring \
---keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+--keyring /etc/apt/keyrings/hashicorp-archive-keyring.gpg \
 --fingerprint
 
 # Add HashiCorp repository to sources list
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+echo "deb [signed-by=/etc/apt/keyrings/hashicorp-archive-keyring.gpg] \
 https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
 sudo tee /etc/apt/sources.list.d/hashicorp.list
 
@@ -25,5 +27,5 @@ sudo apt update
 # Install Terraform
 sudo apt-get install terraform -y
 
-## Verify installation
+# Verify installation
 terraform -v
