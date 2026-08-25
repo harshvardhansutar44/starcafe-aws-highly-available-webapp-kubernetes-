af51847723a612c1a3ad5b966f165b913e560c8a
harshvardhansutar44 <harshvardhansutar5050@gmail.com>
2026-08-25 17:33:21 +0530

    Update trivy.sh

--- scripts/trivy.sh
+++ scripts/trivy.sh
@@ -8,7 +8,7 @@ sudo apt-get install wget apt-transport-https gnupg lsb-release -y
 wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | gpg --dearmor | sudo tee /usr/share/keyrings/trivy.gpg > /dev/null
 
 # Add the Trivy repository to the sources list
-echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main" | sudo tee -a /etc/apt/sources.list.d/trivy.list
+echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb generic main" | sudo tee -a /etc/apt/sources.list.d/trivy.list
 
 # Update package lists
 sudo apt-get update -y
