e5730b389bfc3f36fbea45aca8ebe83816bd42c1
harshvardhansutar44 <harshvardhansutar5050@gmail.com>
2026-08-25 18:00:05 +0530

    Update eksctl.sh

--- scripts/eksctl.sh
+++ scripts/eksctl.sh
@@ -2,10 +2,10 @@
 # Script to install eksctl on an instance
 
 # Download and extract the latest eksctl binary
-curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+curl --silent --location "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 
 # Move eksctl to /usr/local/bin to make it executable from anywhere
 sudo mv /tmp/eksctl /usr/local/bin
 
 # Verify installation
-kubectl version --client
+eksctl version
